bc2fde28910e992250cf9b883af30bb01f477001
Montii <llh9@yahoo.com>
2021-11-13 20:51:02 -0500

    saving input

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path'); 
+const db = require('./db/db.json');
+const fs = require('fs');
 
 const app = express(); 
 
@@ -11,5 +13,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/notes.html')));
+
+app.get('/api/notes', (req, res) => { res.json(db) });
+
+app.post('/api/notes', (req, res) => {
+
+    const writeToFile = (destination, content) => {
+        fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+        );
+    }
+
+
+    const update = (content, file) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(content);
+                writeToFile(file, parsedData);
+            }
+        });
+    };
+
+    if (req.body && req.body.title && req.body.text) {
+        console.info(`${req.method} request received to upvote a review`);
+        let newData = req.body;
+        update(newData, './db/db.json')
+        
+    }else {
+        res.status(500).json('Review ID not found');
+    }
+})
 
 app.listen(PORT, () => console.log(`Serving static asset routes on port ${PORT}!`));
